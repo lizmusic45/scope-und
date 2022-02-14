21dbe96e8dd42abf05a071b738a057481fc70d0e
lizmusic45 <95108244+lizmusic45@users.noreply.github.com>
2022-02-14 10:32:22 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,37 +1,92 @@
-
+import PySimpleGUI as sg
 import matplotlib
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import PySimpleGUI as sg
+
+
 
 # set the theme for the screen/window
 sg.theme("DarkTanBlue")
 # define layout
-options = [[sg.Frame('Channels', [[sg.Radio('Ch1', 'rd_channels', key='Ch1'),
-                                            sg.Radio('Ch2', 'rd_channels', key='Ch2'),
-                                            sg.Radio('Ch2', 'rd_channels', key='Ch3'),
-                                            sg.Radio('Ch4', 'rd_channels', key='Ch4')]],title_color='yellow',
-                     border_width=10)],
-           [sg.Frame('Triggers', [[sg.Radio('1', 'rd_triggers', key='1'),
-                                   sg.Radio('2', 'rd_triggers', key='2'),
-                                   sg.Radio('3', 'rd_triggers', key='3'),
-                                   sg.Radio('4', 'rd_triggers', key='4')]],title_color='yellow',
-           border_width = 10)],
+options = [
+            [sg.Frame('Channels',
+                [
+                    [sg.Checkbox('Ch1', key='Ch1'),
+                     sg.Checkbox('Ch2', key='Ch2'),
+                     sg.Checkbox('Ch3', key='Ch3'),
+                     sg.Checkbox('Ch4', key='Ch4')]
+                ],
+            title_color='yellow',
+            border_width=10)],
 
-           [sg.Frame('Vertical', [[sg.Checkbox('Onion', key='Onion Sauce'),
-                                             sg.Checkbox('Paprika', key='Paprika'),
-                                             sg.Checkbox('Schezwan', key='Schezwan'),
-                                             sg.Checkbox('Tandoori', key='Tandoori')]], title_color='yellow',
-                     border_width=10)],
-[sg.Frame('Functions', [[sg.Checkbox('Peak-To-Peak', key='Peak-To-Peak'),
-                                             sg.Checkbox('Max', key='Max'),
-                                             sg.Checkbox('Min', key='Min'),
-                                             sg.Checkbox('Frequency', key='Frequency'),
-                                             sg.Checkbox('RMS', key='RMS')]], title_color='yellow',
-                     border_width=10)],
-           [sg.Button('Start', font=('Times New Roman', 12)),
-           sg.Button('Close', font=('Times New Roman', 12))]]
+            [sg.Frame('Vertical',
+                [
+                    [sg.Slider(range=(0.5, 5), orientation='v', size=(5, 28), default_value=2, resolution=1.5,
+                               tick_interval=1.5, disable_number_display=True, key='Horiz1'),
+                     sg.Slider(range=(0.5, 5), orientation='v', size=(5, 28), default_value=2, resolution=1.5,
+                               tick_interval=1.5, disable_number_display=True, key='Horiz2'),
+                     sg.Slider(range=(0.5, 5), orientation='v', size=(5, 28), default_value=2, resolution=1.5,
+                               tick_interval=1.5, disable_number_display=True, key='Horiz3'),
+                     sg.Slider(range=(0.5, 5), orientation='v', size=(5, 28), default_value=2, resolution=1.5,
+                               tick_interval=1.5, disable_number_display=True, key='Horiz4')],
+
+                    [sg.Spin(values = ('10mV', '100mV', '1V'), size=(6,18), initial_value='1V',
+                             font=('Helvetica', 8), key='Horiz1Scale'),
+                     sg.Spin(values = ('10mV', '100mV', '1V'), size=(6,18), initial_value='1V',
+                             font=('Helvetica', 8), key='Horiz2Scale'),
+                     sg.Spin(values = ('10mV', '100mV', '1V'), size=(6,18), initial_value='1V',
+                             font=('Helvetica', 8), key='Horiz3Scale'),
+                     sg.Spin(values = ('10mV', '100mV', '1V'), size=(6,18), initial_value='1V',
+                             font=('Helvetica', 8), key='Horiz4Scale')]
+                ],
+            title_color='yellow',
+            border_width=10)],
+
+            [sg.Frame('Triggers',
+                [
+                    [sg.Radio('Ch1', 'rd_triggers', key='Trig1'),
+                     sg.Radio('Ch2', 'rd_triggers', key='Trig2'),
+                     sg.Radio('Ch3', 'rd_triggers', key='Trig3'),
+                     sg.Radio('Ch4', 'rd_triggers', key='Trig4')],
+
+                    [sg.Text('Rise'), sg.Radio('', 'rd_triggers', key='Rise'),
+                     sg.Text('Fall'), sg.Radio('', 'rd_triggers', key='Fall')],
+
+                    [sg.Text('Low Level'), sg.Input(size=(4,18), key='LowL'),
+                     sg.Text('High Level'), sg.Input(size=(4, 18), key='HighL')]
+                ],
+            title_color='yellow',
+            border_width = 10)]
+         ]
+
+#---------------Data that Alex will supply with his code.  this is just a data set for an example ------------------
+ch1fns = ['Ch1', '4.0 V', '1 Hz', '2.83 V', '-2.0 V', '2.0 V']
+ch2fns = ['Ch2', '-', '-', '-', '-', '-']
+ch3fns = ['Ch3', '-', '-', '-', '-', '-']
+ch4fns = ['Ch4', '-', '-', '-', '-', '-']
+
+#------------------the table headings-----------------------------------------------------
+headings = ['CH', 'Pk-to-Pk', 'Freq', 'RMS', 'Min', 'Max']
+
+fns = [
+        sg.Button('Submit', font=('Times New Roman', 12)),
+        sg.Button('Close', font=('Times New Roman', 12)),
+
+        sg.Frame('Functions',
+            [
+                [sg.Table(values=([ch1fns, ch2fns, ch3fns, ch4fns]), headings=headings,
+                          auto_size_columns=False, col_widths=[8,8,8,8,8,8], num_rows=4,
+                          row_colors=[(0,'blue'), (1,'red'), (2,'green'), (3,'orange')],
+                          hide_vertical_scroll=True, justification='center')
+                ]
+            ],
+        pad=((200,0),(0,0)),
+        title_color='yellow',
+        border_width=10)
+      ]
+
 choices = [[sg.Frame('Oscilloscope 4Channels', layout=options)]]
 
 
@@ -54,27 +109,29 @@ def draw_figure(canvas, figure):
 # ------------------------------- Beginning of GUI CODE -------------------------------
 
 # Create layout with two columns using precreated frames
-layout = [[sg.Column(choices), (sg.Canvas(key='-CANVAS-'))]]
+layout = [[sg.Column(choices), (sg.Canvas(key='-CANVAS-'))]],fns
 
 # create the form and show it without the plot
-window = sg.Window('Demo Application - 4 Channel Oscilloscope', layout, location=(100,100), finalize=True)
+window = sg.Window('Demo Application - 4 Channel Oscilloscope', layout, location=(100, 100), finalize=True)
 
 # add the plot to the window
 fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
+
+# Define Window
+#window = sg.Window("Column and Frame", layout)
+
 # Read  values entered by user
 event, values = window.read()
-# access all the values and if selected add them to a string
-strx = ""
-for val in values:
-    if window.FindElement(val).get() == True:
-        strx = strx + " " + val + ","
 
 while True:
     event, values = window.read()  # Read  values entered by user
     if event == sg.WIN_CLOSED:  # If window is closed by user terminate While Loop
         break
-    if event == 'Start':  # If submit button is clicked display chosen values
+    if event == 'Submit':  # If submit button is clicked display chosen values
         window['options'].update(strx)  # output the final string
+    if event == 'Close':
+        window['options'].update(strx)
+       # break
 # Close Window
 window.close()
