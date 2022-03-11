a97853862977c49c41e87b51e08dad7538ae8738
lizmusic45 <95108244+lizmusic45@users.noreply.github.com>
2022-03-11 08:54:59 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -13,11 +13,18 @@ sg.theme("DarkTanBlue")
 # define layout
 
 PORT_list = list_ports.comports()
-length = len(PORT_list)
+PORTlen = len(PORT_list)
 
-PORTname_list = [PORT_list[i][1] for i in range(length)]
+PORTname_list = [PORT_list[i][1] for i in range(PORTlen)]
 PORTname_list.sort()
 PORTname_list.insert(0,'')
+
+VertList = ('10mV', '100mV', '1V')
+VertListMultiplier = (2, 1, 0) #in volts, so 10mV = 10^-2, 100mV = 10^-1, 1V = 10^0
+TrigList = ('mV', 'V')
+TrigListMultiplier = (3, 0) #10^-3, 10^0
+HorizList = ('10us', '100us', '1ms', '10ms', '100ms')
+HorizListMultiplier = (1, 2, 3, 4, 5) #in us, so 10us = 10^1, 100us = 10^2....etc
 
 serialportselect=[sg.Text('Serial'), sg.Combo(values=PORTname_list, size=(30,1), readonly=True,
                                               default_value='', enable_events=True, key='Serial')]
@@ -25,7 +32,7 @@ serialportselect=[sg.Text('Serial'), sg.Combo(values=PORTname_list, size=(30,1),
 options = [
             [sg.Frame('Channels',
                 [
-                    [sg.Checkbox('Ch1', key='Ch1'),
+                    [sg.Checkbox('Ch1', default=True, key='Ch1'),
                      sg.Checkbox('Ch2', key='Ch2'),
                      sg.Checkbox('Ch3', key='Ch3'),
                      sg.Checkbox('Ch4', key='Ch4')]
@@ -35,51 +42,50 @@ options = [
 
             [sg.Frame('Vertical',
                 [
-                    [sg.Slider(range=(0.5, 5), orientation='v', size=(5, 28), default_value=2, resolution=1.5,
-                               tick_interval=1.5, disable_number_display=True, key='Horiz1'),
-                     sg.Slider(range=(0.5, 5), orientation='v', size=(5, 28), default_value=2, resolution=1.5,
-                               tick_interval=1.5, disable_number_display=True, key='Horiz2'),
-                     sg.Slider(range=(0.5, 5), orientation='v', size=(5, 28), default_value=2, resolution=1.5,
-                               tick_interval=1.5, disable_number_display=True, key='Horiz3'),
-                     sg.Slider(range=(0.5, 5), orientation='v', size=(5, 28), default_value=2, resolution=1.5,
-                               tick_interval=1.5, disable_number_display=True, key='Horiz4')],
+                    [sg.Slider(range=(1, 5), orientation='v', size=(5, 28), default_value=1, resolution=2,
+                               tick_interval=2, disable_number_display=True, pad=(11, 0), key='Vert1'),
+                     sg.Slider(range=(1, 5), orientation='v', size=(5, 28), default_value=1, resolution=2,
+                               tick_interval=2, disable_number_display=True, pad=(11, 0), key='Vert2'),
+                     sg.Slider(range=(1, 5), orientation='v', size=(5, 28), default_value=1, resolution=2,
+                               tick_interval=2, disable_number_display=True, pad=(11, 0), key='Vert3'),
+                     sg.Slider(range=(1, 5), orientation='v', size=(5, 28), default_value=1, resolution=2,
+                               tick_interval=2, disable_number_display=True, pad=(11, 0), key='Vert4')],
 
-                    [sg.Spin(values = ('10mV', '100mV', '1V'), size=(6,18), initial_value='1V',
-                             font=('Helvetica', 8), key='Horiz1Scale'),
-                     sg.Spin(values = ('10mV', '100mV', '1V'), size=(6,18), initial_value='1V',
-                             font=('Helvetica', 8), key='Horiz2Scale'),
-                     sg.Spin(values = ('10mV', '100mV', '1V'), size=(6,18), initial_value='1V',
-                             font=('Helvetica', 8), key='Horiz3Scale'),
-                     sg.Spin(values = ('10mV', '100mV', '1V'), size=(6,18), initial_value='1V',
-                             font=('Helvetica', 8), key='Horiz4Scale')]
+                    [sg.Spin(values = VertList, size=(6,18), initial_value='1V',
+                             font=('Helvetica', 8), key='Vert1Scale'),
+                     sg.Spin(values = VertList, size=(6,18), initial_value='1V',
+                             font=('Helvetica', 8), key='Vert2Scale'),
+                     sg.Spin(values = VertList, size=(6,18), initial_value='1V',
+                             font=('Helvetica', 8), key='Vert3Scale'),
+                     sg.Spin(values = VertList, size=(6,18), initial_value='1V',
+                             font=('Helvetica', 8), key='Vert4Scale')]
                 ],
             title_color='yellow',
             border_width=10)],
 
             [sg.Frame('Triggers',
                 [
-                    [sg.Radio('Ch1', 'rd_triggers', key='Trig1'),
+                    [sg.Radio('Ch1', 'rd_triggers', default=True, key='Trig1'),
                      sg.Radio('Ch2', 'rd_triggers', key='Trig2'),
                      sg.Radio('Ch3', 'rd_triggers', key='Trig3'),
                      sg.Radio('Ch4', 'rd_triggers', key='Trig4')],
 
-                    [sg.Text('Rise'), sg.Radio('', 'rd_risefall', key='Rise'),
-                     sg.Text('Fall'), sg.Radio('', 'rd_risefall', key='Fall')],
+                    [sg.Text('Edge:'), sg.Radio('Rise', 'rd_edge', default=True, key='Rise'),
+                     sg.Radio('Fall', 'rd_edge', key='Fall')],
 
-                    [sg.Text('Low'), sg.Input(size=(3,18), default_text='0', key='LowL'),
-                     sg.Spin(values=('V', 'mV'), initial_value='V', size=(3,18), key='LowLScale'),
-                     sg.Text('High'), sg.Input(size=(3, 18), default_text='10', key='HighL'),
-                     sg.Spin(values=('V','mV'), initial_value='V', size=(3,18), key='HighLScale')]
+                    [sg.Text('Level:'), sg.Input(size=(4,18), default_text='10',
+                                                 enable_events=True, key='TrigLevel'),
+                     sg.Spin(values=TrigList, initial_value='mV', size=(3,18), key='TrigLevelScale')]
                 ],
             title_color='yellow',
             border_width = 10)],
 
             [sg.Frame('Horizontal',
                 [
-                    [sg.Slider(range=(2, 10), orientation='h', size=(15, 25), default_value=2, resolution=2,
-                               tick_interval=2, disable_number_display=True, key='HScale'),
-                     sg.Spin(values=('1us', '100us', '1ms', '10ms', '100ms'), size=(12, 20), initial_value='1ms',
-                             font=('Helvetica', 8), key='Horiz1Scale')]
+                    [sg.Slider(range=(2, 8), orientation='h', size=(15, 25), default_value=2, resolution=2,
+                               tick_interval=2, disable_number_display=True, key='Horiz'),
+                     sg.Spin(values=HorizList, size=(12, 20), initial_value='1ms',
+                             font=('Helvetica', 8), key='HorizScale')]
                 ],
             title_color='yellow',
             border_width = 10)]
@@ -159,13 +165,69 @@ while True:
         PORT_name=PORT_list[PORT_index][0]
 
         arduino = serial.Serial(PORT_name, baudrate=9600, timeout=0.1)
-        #------------do some stuff here to change COM setting-------------#
+
+    if event == 'TrigLevel' and values['TrigLevel'] and (values['TrigLevel'][-1] not in ('0123456789.')
+                                                         or len(values['TrigLevel']) > 4):
+        window['TrigLevel'].update(values['TrigLevel'][:-1])
 
 
     if event == 'Submit':  # If submit button is clicked display chosen values
+
         window['settings'].update(settings_list)  # output the final string
 
-        #---code to send settings_list to the serial port---------#
+        #---formatting the data to send over the serial---------#
+        V = [int(settings_list['Vert1']*settings_list['Ch1']), int(settings_list['Vert2']*settings_list['Ch2']),
+             int(settings_list['Vert3']*settings_list['Ch3']), int(settings_list['Vert4']*settings_list['Ch4'])]
+
+        VMult = [VertListMultiplier[VertList.index(settings_list['Vert1Scale'])],
+                 VertListMultiplier[VertList.index(settings_list['Vert2Scale'])],
+                 VertListMultiplier[VertList.index(settings_list['Vert3Scale'])],
+                 VertListMultiplier[VertList.index(settings_list['Vert4Scale'])]]
+
+        H = int(settings_list['Horiz'])
+
+        HMult = HorizListMultiplier[HorizList.index(settings_list['HorizScale'])]
+
+        TrigCh = [settings_list['Trig1'], settings_list['Trig2'], settings_list['Trig3'],
+                  settings_list['Trig4']].index(1)
+
+        TrigEdge = [settings_list['Fall'], settings_list['Rise']].index(1)
+
+        TrigLevelMult = TrigListMultiplier[TrigList.index(settings_list['TrigLevelScale'])]
+
+        if (len(settings_list['TrigLevel']) < 4):
+            if ('.' not in settings_list['TrigLevel']):
+                TrigLevel = settings_list['TrigLevel'] + '.'
+            else:
+                TrigLevel = settings_list['TrigLevel']
+            while len(TrigLevel) < 4:
+                TrigLevel = TrigLevel + '0'
+        else:
+            TrigLevel = settings_list['TrigLevel']
+
+        TrigLevelSet = [TrigLevel[k] for k in range(len(TrigLevel))]
+
+        Settings = V + VMult + [H] + [HMult] + [TrigCh] + [TrigEdge] + [TrigLevelMult] + TrigLevelSet
+
+        SendSettings = np.array(Settings)
+        SendSettingsbyte = SendSettings.astype(bytes)
+
+        print(SendSettingsbyte)
+
+        arduino.write(b's')
+
+        arduino.read_until(b's')
+
+        for k in range(len(SendSettingsbyte)):
+            arduino.write(SendSettingsbyte[k])
+
+        arduino.write(b'e')
+
+
+
+
+
+
 
 
     if event == 'Close':
