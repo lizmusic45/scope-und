03e892d404986a029eb05b50c20ff4a8c26939c7
lizmusic45 <95108244+lizmusic45@users.noreply.github.com>
2022-03-15 10:23:29 -0500

    Update spi_ADS_8634_SerialMonitorTest.ino

--- spi_ADS_8634_SerialMonitorTest.ino
+++ spi_ADS_8634_SerialMonitorTest.ino
@@ -12,14 +12,14 @@
 
 const int CS = 10;                                                      //digital pin for chip select
 const int buff = 5;                                                     //number of samples for the buffer 
-double trig = 0.1;                                                      //trigger to start readings in volts
+double trig = 2.0;                                                      //trigger to start readings in volts
 const int Vref = 2.5;                                                   //internal reference 2.5V
 const int clk = 20000000;                                               //max clock speed for ADS 8634 chip 20Mhz
-int horiz = 500;                                                        //horizontal division user input in us - min 500us to get 200ksps 
+int horiz = 5000;                                                       //horizontal division user input in us - min 500us to get 200ksps 
 int horizDiv = 5;                                                       //the number of horizontal divisions in serial plotter (half of the full set...?)
 int sampleTime = ((horiz*horizDiv)/buff);                               //time between samples in useconds
 int sample0Time = 0;                                                    //set the initial sample read to zero 
-byte channelSelection = {B10000000, B00100000, B00001000, B00000010};   //Ch0 x Ch1 x Ch2 x Ch3 x
+byte channelSelection[4] = {B10000000, B00100000, B00001000, B00000010};//Ch0 x Ch1 x Ch2 x Ch3 x
 int channelNumber = 0;                                                  //index for channel number selection
 
 typedef struct
@@ -57,18 +57,14 @@ void setup() {
 
   //send chip settings writeSPI(reg, setting)  reg bits:  R6 R5 R4 R3 R2 R1 R0 R/W  Write = 0
 
-  digitalWrite(CS, LOW);  //check how long digitalWrite takes - long enough for td(CS-DO) = 40 ns?
-    //use power-up/power-down feature...?  maybe useful after data set on computer...?  
+    //check how long digitalWrite takes - long enough for td(CS-DO) = 40 ns?
     //AL/PD pin is floating - is this okay? YES if programmed as a power-down input
     //register address: 06h, default: 00001000  (Aux-Config: 0000 |AL_PD=1 for power-down| |IntVref=1 to use internal ref| |TempSensor=0 not using temp sensor| 0)
   writeSPI(B00001100, B00001100);  //register is 7-bit address, MSB is R/W: Write = 0
-  digitalWrite(CS, HIGH);
-
-  digitalWrite(CS, LOW);
-    //auto scan  - what should default be...?  always scanning all four is easiest...?
+ 
+    //auto scan channels - what should default be...?  always scanning all four is easiest...?
     //register: 0Ch, default: 00000000 - results in channel 0 being selected  (Auto-Md Ch-Sel: Ch0 X Ch1 X Ch2 X Ch3 X)
   writeSPI(B00011000, channelSelection[channelNumber]);
-  digitalWrite(CS, HIGH);
 
   SPI.endTransaction();
   
@@ -79,6 +75,10 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   SPI.beginTransaction(SPISettings(clk, MSBFIRST, SPI_MODE0));
+
+    //set to auto-scan
+    //register 05h, default:  00000000 - keeps current settings
+  writeSPI(B00001010, B00000000);
 
   do {
     intervalTimer.restart();
@@ -146,11 +146,11 @@ void readSPI(int arrayNum) {
   digitalWrite(CS, LOW); //turn on the chip select (CS)
 
   //get the 16-bit digital number from the ADC
-  sampleIn[arrayNum].b1 = SPI.transfer(0); //receiving channel number and first 4 MSB
-  sampleIn[arrayNum].b2 = SPI.transfer(0); //receiving the last 8 bits
+  sampleIn[arrayNum].b1 = SPI.transfer(B00000000); //receiving channel number and first 4 MSB
+  sampleIn[arrayNum].b2 = SPI.transfer(B00000000); //receiving the last 8 bits
     
   //process the digital bytes back into the channel number
-  sampleIn[arrayNum].ch = (sampleIn[arrayNum].b1 & B11110000) / 32; //channel number shifted down from 2^7 to 2^2
+  sampleIn[arrayNum].ch = (sampleIn[arrayNum].b1 & B01100000) / 32; //channel number shifted down from 2^6 to 2^2
 
   //process the digital bytes back into the analog voltage
   x = (sampleIn[arrayNum].b1 & B00001111) * 256;  //integer value of shifted up MSBs from 2^3 to 2^11
