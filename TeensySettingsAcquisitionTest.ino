9a0f70231f7ec7fcfd4438e3d106c7d4e939fae5
lizmusic45 <95108244+lizmusic45@users.noreply.github.com>
2022-03-19 10:26:53 -0500

    Update TeensySettingsAcquisitionTest.ino

--- TeensySettingsAcquisitionTest.ino
+++ TeensySettingsAcquisitionTest.ino
@@ -3,6 +3,8 @@ char settings_bytes[19] = {'0','0','0','0','0','0','0','0','0','0','0','0','0','
                                                  //Vert1Scale, Vert2Scale, Vert3Scale, Vert4Scale, Horiz, HorizScale, 
                                                  //TrigCh, TrigType, TrigScale, 
                                                  //TrigLevel[0], TrigLevel[1], TrigLevel[2], TrigLevel[3]]
+String settings_strings;
+
 bool printSettings = 0;
 
 byte chDict[16] = {B00000000, B00000010, B00001000, B00001010, B00100000, B00100010, B00101000, B00101010, 
@@ -34,6 +36,8 @@ void loop() {
       SerialUSB1.readBytesUntil('e', settings_bytes, 19);
       start_byte = '0';
       printSettings = 1;
+      
+      settings_strings=String(settings_bytes);
     }
 
     if (start_byte == 'a') {
@@ -48,12 +52,12 @@ void loop() {
     Serial.print("ChList: ");
     Serial.println(chDict[settings_bytes[0]-65]);
     Serial.print("Ch1: ");
-    Serial.print(settings_bytes[1]);
+    Serial.print((int(settings_bytes[1])-48));
     Serial.print(" x 10^-");
     Serial.print(settings_bytes[5]);
     Serial.println(" V");
     Serial.print("Ch2: ");
-    Serial.print(settings_bytes[2]);
+    Serial.print((int(settings_bytes[2])-48));
     Serial.print(" x 10^-");
     Serial.print(settings_bytes[6]);
     Serial.println(" V");
@@ -89,22 +93,22 @@ void loop() {
   }
 
   if (printAcquisition == 1) {
+    //add switch case later
     for(int x=0;x<63;x++){
       float y = float(x)/10;
       //delay(200);
-      Serial.print(sin(y));
+      Serial.print(sin(y)*(int(settings_strings[1])-48));
       Serial.print(",");
       Serial.println(sin(y)*2);
-      
-      SerialUSB1.print(sin(y));
+      SerialUSB1.print(sin(y)*(int(settings_bytes[1])-48)*pow(10,-(int(settings_bytes[5])-48)));
       SerialUSB1.print(',');
-      SerialUSB1.print(sin(y)*2);
+      SerialUSB1.print(sin(y)*2*(int(settings_bytes[2])-48)*pow(10,-(int(settings_bytes[6])-48)));
       SerialUSB1.print(',');
-      SerialUSB1.print(sin(y)*3);
+      SerialUSB1.print(sin(y)*3*(int(settings_bytes[3])-48)*pow(10,-(int(settings_bytes[7])-48)));
       SerialUSB1.print(',');
-      SerialUSB1.print(sin(y)/2);
+      SerialUSB1.print(sin(y)/2*(int(settings_bytes[4])-48)*pow(10,-(int(settings_bytes[8])-48)));
       SerialUSB1.print(',');
-      SerialUSB1.print(x);
+      SerialUSB1.print(x*(int(settings_bytes[9])-48)*pow(10,int(settings_bytes[10])-48));
       SerialUSB1.print('\n');
     }
     SerialUSB1.print('e');
